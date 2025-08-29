a45ed0109aacf53bbe00f45a662bccc5e501310b
Nico-788 <nicolas@DESKTOP-BBAPMM0.localdomain>
2025-08-29 19:20:49 -0300

    Ayuda hecha y arreglo JSON

--- APL/bash/ejercicio1/01-procesador-encuestas.sh
+++ APL/bash/ejercicio1/01-procesador-encuestas.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
 function ayuda() {
-    echo "La ayuda llego"
+    echo -e "\e[1mNAME\e[0m"
+    echo -e "\t01-procesador-encuestas"
+    echo -e "\n\e[1mSYNOPSIS\e[0m"
+    echo -e "\t ./01-procesador-encuestas OPTION DIR OPTION [FILE]"
+    echo -e "\n\e[1mDESCRIPTION\e[0m"
+    echo -e "\tProcesa los archivos .txt que almacenan encuestas el siguiente formato:"
+    echo -e "\n\e[1m\tID_ENCUESTA|FECHA|CANAL|TIEMPO_RESPUESTA|NOTA_SATISFACCION\e[0m"
+    echo -e "\n\tLos argumentos obligatorios para las opciones largas también son obligatorios para las opciones cortas."
+    echo -e "\n\t\e[1m-d, --directorio=DIRECTORY\e[0m"
+    echo -e "\t\truta del directorio con los archivos a procesar."
+    echo -e "\n\t\e[1m-a, --archivo=FILE\e[0m"
+    echo -e "\t\truta completa del archivo JSON de salida. No se puede usar con -p / --pantalla."
+    echo -e "\n\t\e[1m-p, --pantalla\e[0m"
+    echo -e "\t\tmuestra la salida por pantalla. No se puede usar con -a / --archivo."
 }
 
 options=$(getopt -o d:a:ph --l help,pantalla,archivo:,directorio: -- "$@" 2> /dev/null)
 if [ "$?" != "0" ]
 then
-    echo 'Opciones incorrectas'
+    echo 'Opciones incorrectas.'
+    echo "Utilice -h o --help para ayuda"
     exit 1
 fi
 
